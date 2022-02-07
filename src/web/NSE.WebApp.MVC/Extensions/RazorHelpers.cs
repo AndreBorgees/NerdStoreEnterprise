5d6bf25ab2b8f5a855dfbe8d2198e4f8a2d4d1e9
Andre <borges.andree@gmail.com>
2022-02-07 20:37:44 -0300

    Refactored WebApp - Added Brazilian culture by default and extension class created for razer pages

--- src/web/NSE.WebApp.MVC/Extensions/RazorHelpers.cs
+++ src/web/NSE.WebApp.MVC/Extensions/RazorHelpers.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc.Razor;
 using System.Security.Cryptography;
 using System.Text;
+using System.Threading;
 
 namespace NSE.WebApp.MVC.Extensions
 {
@@ -18,15 +19,15 @@ namespace NSE.WebApp.MVC.Extensions
             return sBuilder.ToString();
         }
 
-        //public static string FormatoMoeda(this RazorPage page, decimal valor)
-        //{
-        //    return valor > 0 ? string.Format(Thread.CurrentThread.CurrentCulture, "{0:C}", valor) : "Gratuito";
-        //}
+        public static string FormatCoin(this RazorPage page, decimal value)
+        {
+            return value > 0 ? string.Format(Thread.CurrentThread.CurrentCulture, "{0:C}", value) : "Gratuito";
+        }
 
-        //public static string MensagemEstoque(this RazorPage page, int quantidade)
-        //{
-        //    return quantidade > 0 ? $"Apenas {quantidade} em estoque!" : "Produto esgotado!";
-        //}
-     
+        public static string StockMessage(this RazorPage page, int quantity)
+        {
+            return quantity > 0 ? $"Apenas {quantity} em estoque!" : "Produto esgotado!";
+        }
+
     }
 }
