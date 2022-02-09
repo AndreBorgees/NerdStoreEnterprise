da7b1a05aa7081103e9558ed04cafcd3eb3385ca
Andre <borges.andree@gmail.com>
2022-02-09 16:14:26 -0300

    created mapping and migrations of customer and address

---
+++ src/services/NSE.Cliente.API/Data/CustomersContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NSE.Cliente.API.Models;
+using NSE.Core.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.Cliente.API.Data
+{
+    public sealed class CustomersContext : DbContext, IUnitOfWork
+    {
+        public CustomersContext(DbContextOptions<CustomersContext> options)
+            : base(options)
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+            ChangeTracker.AutoDetectChangesEnabled = false;
+        }
+
+        public DbSet<Customer> customers { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(
+               e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
+                property.SetColumnType("varchar(100)");
+
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(CustomersContext).Assembly);
+        }
+
+        public async Task<bool> Commit()
+        {
+            var success = await base.SaveChangesAsync() > 0;
+
+            return success;
+        }
+    }
+}
---
+++ src/services/NSE.Cliente.API/Data/Mappings/AddressMapping.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NSE.Cliente.API.Models;
+
+namespace NSE.Cliente.API.Data.Mappings
+{
+    public class AddressMapping : IEntityTypeConfiguration<Address>
+    {
+        public void Configure(EntityTypeBuilder<Address> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.PublicPlace)
+                .IsRequired()
+                .HasColumnType("varchar(200)");
+
+            builder.Property(x => x.Number)
+                .IsRequired()
+                .HasColumnType("varchar(50)");
+
+            builder.Property(x => x.Cep)
+                .IsRequired()
+                .HasColumnType("varchar(20)");
+
+            builder.Property(x => x.Complement)
+                .IsRequired()
+                .HasColumnType("varchar(250)");
+
+            builder.Property(x => x.District)
+                .IsRequired()
+                .HasColumnType("varchar(100)");
+
+            builder.Property(x => x.City)
+                .IsRequired()
+                .HasColumnType("varchar(100)");
+
+            builder.Property(x => x.State)
+                .IsRequired()
+                .HasColumnType("varchar(50)");
+
+            builder.ToTable("Addresses");
+        }
+    }
+}
