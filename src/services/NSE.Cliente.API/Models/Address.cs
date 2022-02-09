2b69e753e7eecbac2fb20e5ca6085dbb2ddd0306
Andre <borges.andree@gmail.com>
2022-02-08 21:36:19 -0300

    created customer modeling

---
+++ src/services/NSE.Cliente.API/Models/Address.cs
@@ -0,0 +1,29 @@
+﻿using NSE.Core.DomainObjects;
+using System;
+
+namespace NSE.Cliente.API.Models
+{
+    public class Address : Entity
+    {
+        public Address(string publicPlace, string number, string complement, string district, string cep, string city, string state)
+        {
+            PublicPlace = publicPlace;
+            Number = number;
+            Complement = complement;
+            District = district;
+            Cep = cep;
+            City = city;
+            State = state;
+        }
+
+        public string PublicPlace { get; private set; }
+        public string Number { get; private set; }
+        public string Complement { get; private set; }
+        public string District { get; private set; }
+        public string Cep { get; private set; }
+        public string City { get; private set; }
+        public string State { get; private set; }
+        public Guid CustomerId { get; private set; }
+        public Customer Customer { get; private set; }
+    }
+}
---
+++ src/services/NSE.Cliente.API/Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using NSE.Core.DomainObjects;
+
+namespace NSE.Cliente.API.Models
+{
+    public class Customer : Entity, IAggregateRoot
+    {
+        public Customer(string name, string email, string cpf)
+        {
+            Name = name;
+            Email = email;
+            Cpf = cpf;
+            isRemoved = false;
+        }
+
+        protected Customer(){}
+
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Cpf { get; private set; }
+        public bool isRemoved { get; private set; }
+        public Address Address { get; private set; }
+    }
+}
