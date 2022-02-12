0b219952b1d502f1741da2081f58cab71d349743
Andre Borges Note <borges.andree@gmail.com>
2022-02-12 15:13:22 -0300

    Validation in RegisterCustomerCommand

--- src/services/NSE.Cliente.API/Application/Commands/RegisterCustomerCommand.cs
+++ src/services/NSE.Cliente.API/Application/Commands/RegisterCustomerCommand.cs
@@ -1,9 +1,10 @@
-﻿using NSE.Core.Messages;
+﻿using FluentValidation;
+using NSE.Core.Messages;
 using System;
 
 namespace NSE.Cliente.API.Application.Commands
 {
-    public class RegisterCustomerCommand: Command
+    public class RegisterCustomerCommand : Command
     {
         public RegisterCustomerCommand(Guid id, string name, string email, string cpf)
         {
@@ -18,5 +19,43 @@ namespace NSE.Cliente.API.Application.Commands
         public string Name { get; private set; }
         public string Email { get; private set; }
         public string Cpf { get; private set; }
-    }
+
+        public override bool IsValid()
+        {
+            ValidationResult = new RegisterCustomerValidation().Validate(this);
+            return ValidationResult.IsValid;
+        }
+        public class RegisterCustomerValidation : AbstractValidator<RegisterCustomerCommand>
+        {
+            public RegisterCustomerValidation()
+            {
+                RuleFor(x => x.Id)
+                    .NotEqual(Guid.Empty)
+                    .WithMessage("Id do cliente inválido");
+
+                RuleFor(x => x.Name)
+                   .NotEmpty()
+                   .WithMessage("O nome do cliente não foi informado");
+
+                RuleFor(x => x.Cpf)
+                    .Must(ValidateCpf)
+                    .WithMessage("O CPF informado é inválido");
+
+
+                RuleFor(x => x.Email)
+                    .Must(ValidateEmail)
+                    .WithMessage("O E-mail informado é inválido");
+            }
+
+            public static bool ValidateCpf(string cpf)
+            {
+                return Core.DomainObjects.Cpf.Validate(cpf);
+            }
+
+            public static bool ValidateEmail(string email)
+            {
+                return Core.DomainObjects.Email.Validate(email);
+            }
+        }
+    }   
 }
